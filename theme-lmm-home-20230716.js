e75db2dd3ece7988ccdea968f40385abbbb67017
edinik <edinik@users.noreply.github.com>
2023-07-17 14:52:47 +0000

    修改相册 edinik/techblog@be9969b273e69b8c65c6ee97acc2d64e8a7b6eed

--- theme-lmm-home-20230716.js
+++ theme-lmm-home-20230716.js
@@ -34,7 +34,7 @@ if(albumDom){memoAlbum(6);}
 function memoAlbum(numb){
     let limit = numb || 8;
     var memoUrl = "https://memos.edinik.com/"
-    var galleryUrl = memoUrl+"api/v1/memo/all?rowStatus=NORMAL&limit="+limit+"&tag=相册"
+    var galleryUrl = memoUrl+"api/memo?openId=6fe69b8b-0184-4db8-a689-241a43a2c26a&rowStatus=NORMAL&limit="+limit+"&tag=相册"
     var localalbumUpdated = JSON.parse(localStorage.getItem("albumUpdated")) || '';
     var localalbumData = JSON.parse(localStorage.getItem("albumData")) || '';
     if(localalbumData){
